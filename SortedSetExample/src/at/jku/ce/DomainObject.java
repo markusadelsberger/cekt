0103250f93e8a0958297d961edd22b8b5f30c665
Arthur <markus.adelsberger@gmail.com>
2011-05-04 19:38:26 +0200

    <Tag 5> Arthur: getter und setter fuer Name

--- SortedSetExample/src/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/at/jku/ce/DomainObject.java
@@ -27,5 +27,13 @@ public class DomainObject {
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
+	
+	public String getName() {
+		return name; 
+	}
+	
+	public void setName(String name) {
+		this.name = name; 
+	}
 
 }
