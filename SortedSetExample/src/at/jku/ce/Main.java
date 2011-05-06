9f3d5026e4cb63dd11e6c010f822132ca12cf2f7
Barbara <zaunmayr@gmx.at>
2011-05-06 16:30:41 +0200

    <Tag12b> letzte Aenderungen

--- SortedSetExample/src/at/jku/ce/Main.java
+++ SortedSetExample/src/at/jku/ce/Main.java
@@ -10,7 +10,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Set<DomainObject> objectSet = new HashSet<DomainObject>();
+		Set<DomainObject> objectSet = new TreeSet<DomainObject>(new DomainObjectComparator());
 		objectSet.add(new DomainObject("Helga", ""));
 		objectSet.add(new DomainObject("Walter", ""));
 		objectSet.add(new DomainObject("Hannah", ""));
@@ -18,7 +18,7 @@ public class Main {
 		objectSet.add(new DomainObject("Claudia", ""));
 		
 		for (DomainObject domainObject : objectSet) {
-		  LOGGER.debug(domainObject.uuid);
+		  LOGGER.debug(domainObject.toString());
 		}
 	}
 }
